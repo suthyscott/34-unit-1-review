95538456a11e80f04142bec5e7aebcd5f3a4744e
suthyscott <sutherlandscott21@gmail.com>
2023-02-15 16:05:08 -0700

    using .map, styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,37 @@
-import './App.css';
-import Profile from './components/Profile';
+import "./App.css"
+import Profile from "./components/Profile"
+import { useState } from "react"
 
 const App = () => {
-  const user = {
-    firstName: 'Scott',
-    lastName: 'Sutherland'
-  }
+    const [profiles, setProfiles] = useState([
+        {
+            firstName: "Scott",
+            lastName: "Sutherland"
+        },
+        {
+            firstName: "Kyle",
+            lastName: "Baugh"
+        },
+        {
+            firstName: "Joely",
+            lastName: "Vernier"
+        }
+    ])
 
-  const userTwo = {
-    firstName: 'Kyle',
-    lastName: 'Baugh'
-  }
+    const profilesDisplay = profiles.map((profileObj) => {
+      console.log(profileObj)
+      return <Profile info={profileObj}/>
+    })
 
-
-  return (
-    <div>
-      <Profile info={user} />
-      <Profile info={userTwo} />
-    </div>
-  );
+    return (
+        <div>
+            {profilesDisplay}
+            {/* {profiles.map(profileObj => {
+                console.log(profileObj)
+                return <Profile info={profileObj} />
+            })} */}
+        </div>
+    )
 }
 
-export default App;
+export default App
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react"
+import './Profile.css'
 
 const Profile = (props) => {
     const [favColor, setFavColor] = useState('TBD')
     // let favColor = 'TBD'
-    console.log("Profile.jsx", props)
+    // console.log("Profile.jsx", props)
+
     return(
-        <div>
-            <p>First name: {props.info.firstName}</p>
+        <div className="profile-container">
+            <p style={{color: 'green', backgroundColor: 'red'}}>First name: {props.info.firstName}</p>
             <p>Last name: {props.info.lastName}</p>
             <p>Favorite Color: {favColor}</p>
             <input placeholder="Enter your favorite color here" onChange={e => setFavColor(e.target.value)}/>
